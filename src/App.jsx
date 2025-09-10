36a748d0e589f37791964d6f9b284f7fe7c1bf46
Deniz Koc <dkocusa@outlook.com>
2025-09-10 14:10:10 -0500

    WIP: update App.jsx before switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import "./App.css"
+import { Nav } from "./components/nav/Nav"
+import { Order } from "./components/order/Order"
 
 function App() {
-	return (
-		<Routes>
-			<Route
-				path="/"
-				element={
-					<>
-						{/* <NavBar /> */}
-						<Outlet />
-					</>
-				}>
-				<Route index element={<div>Order</div>} />
-			</Route>
-		</Routes>
-	)
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Nav />
+            <Outlet />
+          </>
+        }
+      >
+        
+        <Route index element={<Order />} />
+
+        
+        <Route path="cart" element={<div>Shopping Cart</div>} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
