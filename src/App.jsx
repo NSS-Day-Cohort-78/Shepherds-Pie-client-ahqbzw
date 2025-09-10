b10cc90470dd61f010c92166f366e8f3b5f5440d
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-10 12:00:37 -0500

    added trash button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import "./App.css"
 
 function App() {
-  return <div className="App">Shepherd's Pies</div>;
+	return <div className="App">Shepherd's Pies</div>
 }
 
-export default App;
+export default App
