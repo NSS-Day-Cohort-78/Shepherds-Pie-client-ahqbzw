51b61db5aba667cabbeec75335a3e21a2290c42f
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-10 13:23:43 -0500

    router set  up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Outlet, Route, Routes } from "react-router-dom"
 import "./App.css"
 
 function App() {
-	return <div className="App">Shepherd's Pies</div>
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<>
+						{/* <NavBar /> */}
+						<Outlet />
+					</>
+				}>
+				<Route index element={<div>Order</div>} />
+			</Route>
+		</Routes>
+	)
 }
 
 export default App
