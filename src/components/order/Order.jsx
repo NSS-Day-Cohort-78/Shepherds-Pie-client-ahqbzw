e0b4a5aefc0efa5cf8844199bbb749da335ccfe1
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-11 11:37:35 -0500

    employees-dropdown

--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -2,16 +2,19 @@ import React, { useState } from "react"
 import "./order.css"
 import { ChooseSizes } from "./ChooseSizes"
 import { ChooseSauces } from "./ChooseSauces"
+import { ServerDropdown } from "./ServerDropdown"
 
 export const Order = () => {
     const [sizes, setSizes] = useState([])
     const [sauces, setSauces] = useState([])
+    const [employees, setEmployees] = useState([])
 
     return (
         <form>
             <h1>What kind of pizza would you like?</h1>
             <ChooseSizes sizes={sizes} setSizes={setSizes} />
             <ChooseSauces sauces={sauces} setSauces={setSauces} />
+            <ServerDropdown employees={employees} setEmployees={setEmployees} />
         </form>
     )
 }
