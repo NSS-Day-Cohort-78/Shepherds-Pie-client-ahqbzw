3e4d01637b441804a2d7cf31f0e3e51b5eecafe4
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-12 09:06:44 -0500

    working on how to hold the state for our toppings

--- src/components/order/ChooseToppings.jsx
+++ src/components/order/ChooseToppings.jsx
@@ -1,20 +1,51 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { getToppings } from "../services/ToppingsServices"
 
-export const ChooseToppings = ({ toppings, setToppings }) => {
-  useEffect(() => {
-    getToppings().then(setToppings)
-  }, [])
+export const ChooseToppings = ({
+    pizzaToppings,
+    setPizzaToppings,
+    toppings,
+    setToppings
+}) => {
+    useEffect(() => {
+        getToppings().then(setToppings)
+    }, [])
 
-  return (
-    <fieldset>
-      <legend>Choose Toppings</legend>
-      {toppings.map(t => (
-        <div key={t.id}>
-          <input type="checkbox" id={`topping-${t.id}`} value={t.id} />
-          <label htmlFor={`topping-${t.id}`}>{t.name}</label>
-        </div>
-      ))}
-    </fieldset>
-  )
+    // single pizza topping state
+    const [pizzaTop, setPizzaTop] = useState({
+        pizzaId: 0,
+        toppingId: 0
+    })
+
+    // add single pizza topping to array of all toppings
+
+    useEffect(() => {
+        const copyPizzaToppings = [...pizzaToppings, pizzaTop]
+        setPizzaToppings(copyPizzaToppings)
+    }, [pizzaTop])
+
+    // it is selecting a topping on checked and uncheck
+    const handleChange = e => {
+        const copyPizzaTop = { ...pizzaTop }
+        copyPizzaTop[e.target.name] = e.target.value
+        setPizzaTop(copyPizzaTop)
+    }
+
+    return (
+        <fieldset>
+            <legend>Choose Toppings</legend>
+            {toppings.map(t => (
+                <div key={t.id}>
+                    <input
+                        onChange={handleChange}
+                        type="checkbox"
+                        name="toppingId"
+                        id={`topping-${t.id}`}
+                        value={t.id}
+                    />
+                    <label htmlFor={`topping-${t.id}`}>{t.name}</label>
+                </div>
+            ))}
+        </fieldset>
+    )
 }
--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./order.css"
 import { ChooseSizes } from "./ChooseSizes"
 import { ChooseSauces } from "./ChooseSauces"
@@ -15,12 +15,30 @@ export const Order = () => {
     const [employees, setEmployees] = useState([])
     const [drivers, setDrivers] = useState([])
 
+    const [pizza, setPizza] = useState({
+        sizeId: 0,
+        cheeseId: 0,
+        sauceId: 0
+    })
+
+    // single pizza topping state
+
+    // array of all toppings we choose
+    const [pizzaToppings, setPizzaToppings] = useState([])
+
+    // add single pizza topping to array of all toppings
+
     return (
         <form>
             <h1>What kind of pizza would you like?</h1>
             <ChooseSizes sizes={sizes} setSizes={setSizes} />
             <ChooseSauces sauces={sauces} setSauces={setSauces} />
-            <ChooseToppings toppings={toppings} setToppings={setToppings} />
+            <ChooseToppings
+                pizzaToppings={pizzaToppings}
+                setPizzaToppings={setPizzaToppings}
+                toppings={toppings}
+                setToppings={setToppings}
+            />
             <ServerDropdown employees={employees} setEmployees={setEmployees} />
             <DeliveryButton orderType={orderType} setOrderType={setOrderType} />
             <DriverDropdown
