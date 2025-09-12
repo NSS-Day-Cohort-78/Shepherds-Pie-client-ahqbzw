7e1ff6c9c641cd53dfe106870ecfc03bf2099b5c
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-12 10:54:02 -0500

    store current sauce id

--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -18,6 +18,7 @@ export const Order = () => {
     const [drivers, setDrivers] = useState([])
     const [isChecked, setIsChecked] = useState([])
     const [currentSize, setCurrentSize] = useState(0)
+    const [currentSauce, setCurrentSauce] = useState(0)
 
     const [pizza, setPizza] = useState({
         sizeId: 0,
@@ -47,7 +48,11 @@ export const Order = () => {
                 sizes={sizes}
                 setSizes={setSizes}
             />
-            <ChooseSauces sauces={sauces} setSauces={setSauces} />
+            <ChooseSauces
+                setCurrentSauce={setCurrentSauce}
+                sauces={sauces}
+                setSauces={setSauces}
+            />
             <ChooseCheeses cheeses={cheeses} setCheeses={setCheeses} />
             <ChooseToppings
                 toppings={toppings}
