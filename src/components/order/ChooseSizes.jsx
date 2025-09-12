92fa6227ba3ba976d50ad3c34f24414304a3cbe4
Deniz Koc <dkocusa@outlook.com>
2025-09-12 11:44:26 -0500

    Revert "Deniz nav"

--- src/components/order/ChooseSizes.jsx
+++ src/components/order/ChooseSizes.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect } from "react"
 import { getSizes } from "../services/SizeServices"
 
-export const ChooseSizes = ({ setCurrentSize, sizes, setSizes }) => {
+export const ChooseSizes = ({ sizes, setSizes }) => {
     useEffect(() => {
         getSizes().then(setSizes)
     }, [])
-
-    const handleChange = e => {
-        setCurrentSize(parseInt(e.target.value))
-    }
     return (
         <fieldset>
             <label htmlFor="sizes">Sizes</label>
-            <select onChange={handleChange} name="sizes" id="sizes">
+            <select name="sizes" id="sizes">
                 <option value="0">Choose Size</option>
                 {sizes.map(s => {
                     return (
--- src/components/order/Order.jsx
+++ src/components/order/Order.jsx
@@ -1,76 +1,17 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import "./order.css"
 import { ChooseSizes } from "./ChooseSizes"
 import { ChooseSauces } from "./ChooseSauces"
-import { ServerDropdown } from "./ServerDropdown"
-import { DriverDropdown } from "./DriverDropdown"
-import { ChooseToppings } from "./ChooseToppings"
-import { DeliveryButton } from "./OrderTypeButton"
-import { ChooseCheeses } from "./ChooseCheeses"
-import { AddToOrderButton } from "./AddToOrderButton"
-
-
 
 export const Order = () => {
-    const [orderType, setOrderType] = useState(false)
     const [sizes, setSizes] = useState([])
     const [sauces, setSauces] = useState([])
-    const [cheeses, setCheeses] = useState([])
-    const [toppings, setToppings] = useState([])
-    const [employees, setEmployees] = useState([])
-    const [drivers, setDrivers] = useState([])
-    const [isChecked, setIsChecked] = useState([])
-    const [currentSize, setCurrentSize] = useState(0)
-    const [currentSauce, setCurrentSauce] = useState(0)
-
-    const [pizza, setPizza] = useState({
-        sizeId: 0,
-        cheeseId: 0,
-        sauceId: 0
-    })
-
-    // array of all toppings we choose
-    const [pizzaToppings, setPizzaToppings] = useState([])
-
-    useEffect(() => {
-        const checkedArray = toppings.map(topping => {
-            return {
-                id: topping.id,
-                checked: false
-            }
-        })
-
-        setIsChecked(checkedArray)
-    }, [toppings])
 
     return (
         <form>
             <h1>What kind of pizza would you like?</h1>
-            <ChooseSizes
-                setCurrentSize={setCurrentSize}
-                sizes={sizes}
-                setSizes={setSizes}
-            />
-            <ChooseSauces
-                setCurrentSauce={setCurrentSauce}
-                sauces={sauces}
-                setSauces={setSauces}
-            />
-            <ChooseCheeses cheeses={cheeses} setCheeses={setCheeses} pizza={pizza} setPizza={setPizza} />
-            <ChooseToppings
-                toppings={toppings}
-                setToppings={setToppings}
-                isChecked={isChecked}
-                setIsChecked={setIsChecked}
-            />
-            <ServerDropdown employees={employees} setEmployees={setEmployees} />
-            <DeliveryButton orderType={orderType} setOrderType={setOrderType} />
-            <DriverDropdown
-                drivers={drivers}
-                setDrivers={setDrivers}
-                orderType={orderType}
-            />
-            <AddToOrderButton/>
+            <ChooseSizes sizes={sizes} setSizes={setSizes} />
+            <ChooseSauces sauces={sauces} setSauces={setSauces} />
         </form>
     )
 }
